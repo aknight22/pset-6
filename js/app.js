a57ec325a3d1f2d962e387c79e190a2d2f9cbda5
Angelia Knight <aknight@ucvts.org>
2020-01-03 13:23:04 -0500

    index and java

---
+++ js/app.js
@@ -0,0 +1,7 @@
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("input").value;
+  var addedElement = document.createTextNode(inputValue);
+  li.appendChild(addedElement);
+  document.getElementById("list").appendChild(li);
+}
